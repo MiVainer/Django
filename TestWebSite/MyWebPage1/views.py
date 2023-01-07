1f653a9f762835bd6de8693c0a4d52a2935758bd
Mihail Vainer <mh93@yandex.ru>
2023-01-07 19:06:47 +0700

    Version 1.1

--- TestWebSite/MyWebPage1/views.py
+++ TestWebSite/MyWebPage1/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<center><h2>Hello!!! From Python Django!! </h2></center>")
+    return HttpResponse("<center><h2>Hello!!! From Python Django!! By MiVainer</h2></center>")
